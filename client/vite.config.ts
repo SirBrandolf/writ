4efae474bb3718fdd3e74e4949bf75f269331222
Yashila Barnwal <yashilabarnwal@eduroam-campus-10-36-246-198.campus-dynamic.uwaterloo.ca>
2026-03-22 15:10:38 -0400

    stuff

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5175,
+    allowedHosts: true,
+    proxy: {
+      '/notes': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
